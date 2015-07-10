c85a36568505e32eb77506a943025ff024bfa016
Sergio Bayona <sbayona@martianinteractive.com>
2015-07-10 16:11:38 -0400

    updated file names

--- lib/ivona_speech_cloud.rb
+++ lib/ivona_speech_cloud.rb
@@ -1,4 +1,5 @@
 require "ivona_speech_cloud/version"
 require "ivona_speech_cloud/client"
 require "ivona_speech_cloud/speech"
-require "ivona_speech_cloud/payload"
+require "ivona_speech_cloud/list_voices"
+require "ivona_speech_cloud/inputs"
