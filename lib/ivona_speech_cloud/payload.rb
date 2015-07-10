cc64b97cc8299342b88c7f74cc01a173cc07be5e
Sergio Bayona <sbayona@martianinteractive.com>
2015-07-09 22:07:36 -0400

    namespace it

--- lib/ivona_speech_cloud/payload.rb
+++ lib/ivona_speech_cloud/payload.rb
@@ -1,67 +1,71 @@
-class Payload
+module IvonaSpeechCloud
+  class Payload
 
-  def initialize(text, options)
-    @text = text
-    @options = options
-  end
+    attr_accessor :text, :options
 
-  def create
+    def initialize(text, options={})
+      @text = text
+      @options = options
+    end
+
+    def create
       {
-          "Input": {
-              "Data" => @text
-          },
-          "OutputFormat" => {
-              "Codec" => codec,
-              "SampleRate" => sample_rate,
-          },
-          "Parameters" => {
-              "Rate" => rate,
-              "Volume" => volume,
-              "SentenceBreak" => sentence_break,
-              "ParagraphBreak" => paragraph_break
-          },
-          "Voice" => {
-              "Name" => voice_name,
-              "Language" => language,
-              "Gender" => gender
-          }
+        "Input": {
+          "Data" => text
+        },
+        "OutputFormat" => {
+          "Codec" => codec,
+          "SampleRate" => sample_rate,
+        },
+        "Parameters" => {
+          "Rate" => rate,
+          "Volume" => volume,
+          "SentenceBreak" => sentence_break,
+          "ParagraphBreak" => paragraph_break
+        },
+        "Voice" => {
+          "Name" => voice_name,
+          "Language" => language,
+          "Gender" => gender
+        }
       }.to_json
-  end
+    end
 
     def codec
-      @options.fetch(:codec, "MP3")
+      options.fetch(:codec, "MP3")
     end
 
     def sample_rate
-      @options.fetch(:sample_rate, 22050)
+      options.fetch(:sample_rate, 22050)
     end
 
     def rate
-      @options.fetch(:rate, "medium")
+      options.fetch(:rate, "medium")
     end
 
     def volume
-      @options.fetch(:volume, "medium")
+      options.fetch(:volume, "medium")
     end
 
     def sentence_break
-      @options.fetch(:sentence_break, 500)
+      options.fetch(:sentence_break, 500)
     end
 
     def paragraph_break
-      @options.fetch(:paragraph_break, 800)
+      options.fetch(:paragraph_break, 800)
     end
 
     def voice_name
-      @options.fetch(:voice, "Salli")
+      options.fetch(:voice, "Salli")
     end
 
     def language
-      @options.fetch(:language, "en-US")
+      options.fetch(:language, "en-US")
     end
 
     def gender
-      @options.fetch(:gender, "Female")
+      options.fetch(:gender, "Female")
     end
 
+  end
 end
