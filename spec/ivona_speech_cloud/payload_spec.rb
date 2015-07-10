1890b801a2d03d45c7c1c47850cddab33d36b0b4
Sergio Bayona <sbayona@martianinteractive.com>
2015-07-10 16:14:26 -0400

    updated class names

--- spec/ivona_speech_cloud/payload_spec.rb
+++ spec/ivona_speech_cloud/payload_spec.rb
@@ -1,21 +1,21 @@
 require 'helper'
 
-describe IvonaSpeechCloud::Payload do
+describe IvonaSpeechCloud::SpeechInput do
   
-  subject { IvonaSpeechCloud::Payload.new("hello world!") }
+  subject { IvonaSpeechCloud::SpeechInput.new("hello world!") }
 
   it { expect(subject.text).to eq "hello world!" }
 
   it { expect(subject.options).to eq Hash.new }
 
   describe "with options" do
-    subject { IvonaSpeechCloud::Payload.new("Hello world!", {codec: "OGG"}) }
+    subject { IvonaSpeechCloud::SpeechInput.new("Hello world!", {codec: "OGG"}) }
 
     it { expect(subject.codec).to eq "OGG" }
 
     it { expect(subject.options).to eq({codec: "OGG"}) }
 
-    it { expect(JSON(subject.speech_params)["OutputFormat"]).to eq({
+    it { expect(JSON(subject.params)["OutputFormat"]).to eq({
         "Codec" => "OGG", 
         "SampleRate" => 22050
         })
