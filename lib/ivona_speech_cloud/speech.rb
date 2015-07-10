55b19d4c523a220104310c2c1ea9c2d49113cf84
Sergio Bayona <sbayona@martianinteractive.com>
2015-07-09 20:52:04 -0400

    moved parameter-related methods out

--- lib/ivona_speech_cloud/speech.rb
+++ lib/ivona_speech_cloud/speech.rb
@@ -19,37 +19,6 @@ module IvonaSpeechCloud
       HTTParty.post(uri, body: payload, headers: @client.signed_headers)
     end
 
-
-    def codec
-      @options.fetch(:codec, "MP3")
-    end
-
-    def sample_rate
-      @options.fetch(:sample_rate, 22050)
-    end
-
-    def rate
-      @options.fetch(:rate, "medium")
-    end
-
-    def volume
-      @options.fetch(:volume, "medium")
-    end
-
-    def sentence_break
-      @options.fetch(:sentence_break, 500)
-    end
-
-    def paragraph_break
-      @options.fetch(:paragraph_break, 800)
-    end
-
-    def voice_name
-      @options.fetch(:voice, "salli")
-    end
-
-    def language
-      @options.fetch(:language, "en-US")
     end
 
     def gender
