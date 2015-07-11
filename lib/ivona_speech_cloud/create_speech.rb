46ca73867034baef4d210d284ddf7800a401ec05
Sergio Bayona <sbayona@martianinteractive.com>
2015-07-11 18:22:51 -0400

    added examples of possible options

--- lib/ivona_speech_cloud/create_speech.rb
+++ lib/ivona_speech_cloud/create_speech.rb
@@ -5,12 +5,16 @@ module IvonaSpeechCloud
     include Runner
 
     # Initializes a new CreateSpeech object
+    # options example: 
+    # ["Hello world!"]
+    # ["Hello world!", {:voice_name => "Joey"}]
+    # ["Bonjour le monde", {:gender => "Female", :language => "fr-FR", :voice_name => "Celine"}]
     #
     # @param client [IvonaSpeechCloud::Client]
     # @param text [String]
-    # @param options [Hash]
+    # @param options [Array]
     # @return [IvonaSpeechCloud::CreateSpeech]
-    def initialize(client, *options)
+    def initialize(client, options)
       @client = client
       @options = options
       @path = "/CreateSpeech"
